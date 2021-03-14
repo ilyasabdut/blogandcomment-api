e9be7a6fbf9bded0b6863f382337c61a9f57ef6e
ilyas abduttawab <ilyasabdut@192.168.0.113>
2021-03-14 23:04:14 +0700

    deployed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -31,7 +32,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/blogs.routes.js")(app);
 
-const PORT = 5432 || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
