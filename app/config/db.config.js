36c1c706b19c23821503715655829f6fc9cabe64
ilyas abduttawab <ilyasabdut@192.168.0.113>
2021-03-14 23:11:18 +0700

    deployed

---
+++ app/config/db.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // HOST: "localhost",
+  // USER: "postgres",
+  // PASSWORD: null,
+  // DB: "blogandcomment_database",
+  // dialect: "postgres",
+  HOST: "ec2-54-145-249-177.compute-1.amazonaws.com",
+  USER: "qcqxokofpdqmyo",
+  PASSWORD: "579521c5eb9f6511f1c46887de6623cbeac0a7e2e0eb43597f7f25a49f8a8b16",
+  DB: "d6svsrev9d0vui",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
