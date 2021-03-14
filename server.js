7b9947f66e3ca4d6676bdef32a2a5c34993c4656
ilyas abduttawab <ilyasabdut@192.168.0.113>
2021-03-14 23:45:29 +0700

    deployed

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/blogs.routes.js")(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5432;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
